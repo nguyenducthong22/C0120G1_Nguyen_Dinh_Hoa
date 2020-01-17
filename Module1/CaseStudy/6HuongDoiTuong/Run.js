346852a9d89a8e3a3274b788857541fcf70fa811
Admin <dinhhoa1994@gmail.com>
2020-01-17 11:34:55 +0700

    nhin cai gi??

--- Module1/CaseStudy/6HuongDoiTuong/Run.js
+++ Module1/CaseStudy/6HuongDoiTuong/Run.js
@@ -7,6 +7,14 @@ function addNewCustomer() {
     cus.setBirthdayCustomer(prompt("Enter Birthday Customer (dd/MM/YYYY): "));
     cus.setEmailCustomer(prompt("Enter Email Customer: "));
     cus.setAddressCustomer(prompt("Enter Address Customer: "));
+    cus.setTypeCustomer(prompt("Enter Type Customer: "));
+    cus.setDiscount(prompt("Enter Discout: "));
+    cus.setNumberOfAccompanying(prompt("Enter Number of Accompanying: "));
+    cus.setTypeRoom(prompt("Enter Type Room: "));
+    cus.setRentDays(prompt("Enter Rent Days: "));
+    cus.setTypeService(prompt("Enter Type Service: "));
+    listCostomers.push(cus);
+
 
 
 
