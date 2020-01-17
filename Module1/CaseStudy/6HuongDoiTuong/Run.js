a91f23a227849f5ce5d611ca1b7d197a0930d8eb
Admin <dinhhoa1994@gmail.com>
2020-01-17 11:31:21 +0700

    asdasd

---
+++ Module1/CaseStudy/6HuongDoiTuong/Run.js
@@ -0,0 +1,13 @@
+let listCostomers = [];
+
+function addNewCustomer() {
+    let cus = new Cuscomer();
+    cus.setNameCustomer(prompt("Enter Name Customer"));
+    cus.setIdCard(prompt("Enter Id Card Customer: "));
+    cus.setBirthdayCustomer(prompt("Enter Birthday Customer (dd/MM/YYYY): "));
+    cus.setEmailCustomer(prompt("Enter Email Customer: "));
+    cus.setAddressCustomer(prompt("Enter Address Customer: "));
+
+
+
+}
