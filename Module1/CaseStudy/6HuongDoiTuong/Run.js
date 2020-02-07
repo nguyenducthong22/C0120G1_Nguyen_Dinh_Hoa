08ffc509b3141bd676efeb24accaec502dbc18fe
Admin <dinhhoa1994@gmail.com>
2020-02-07 20:50:45 +0700

    nhin cai gi??

--- Module1/CaseStudy/6HuongDoiTuong/Run.js
+++ Module1/CaseStudy/6HuongDoiTuong/Run.js
@@ -1,10 +1,10 @@
 let listCustomers = [];
+let listEmployee = [];
 let checkEditCustomer = false;
 let checkDeleteCustomer = false;
 let checkDisplayTotalPay = false;
 
 
-// let validateBirthday1 =/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 let validateBirthday = /^([0-2]\d|(3)[0-1])(\/)(((0)\d)|((1)[0-2]))(\/)[1]{1}[9]{1}[3-9]{1}\d{1}|[2]{1}[0]{1}[0-1]{1}\d{1}$/;
 let validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 let validateIdCard = /^\d{9}$/;
@@ -15,7 +15,11 @@ let validateTypeCustomer = /^(diamond|platinium|gold|silver|member)$/;
 let validateTypeRoom = /^(vip|business|normal)$/;
 let validateTypeService = /^(villa|room|house)$/;
 let validateAddressCustomer = /^(danang|hue|quangnam)$/;
-
+let validatePhoneNumber = /^([0-9]{1}[0-9]{9})|(\+84[1-9]{1}[0-9]{8})$/;
+let validateLevelEmployee = /^trungcap|caodang|daihoc$/;
+let validateLocationEmployee = /^letan|phucvu|giamsat|giamdoc$/;
+let validateSalary = /^\d+$/;
+let validateWorkEmployee = /^manager|sale|marketing$/;
 
 function displayMainMenu() {
     let choose = prompt("" +
@@ -24,7 +28,9 @@ function displayMainMenu() {
         "\n3. Display total pays of customer " +
         "\n4. Edit information customer" +
         "\n5. Delete customer" +
-        "\n6. Exit ");
+        "\n6. Add new employee" +
+        "\n7. Display information employee" +
+        "\n8. Exit ");
     switch (choose) {
         case "1":
             addNewCustomer();
@@ -42,6 +48,12 @@ function displayMainMenu() {
             chooseDeleteCustomer();
             break;
         case "6":
+            addNewEmployee();
+            break;
+        case "7":
+            displayEmployee();
+            break;
+        case "8":
             return;
         default:
             alert("Faillll cmnr :(");
@@ -49,6 +61,154 @@ function displayMainMenu() {
             break;
     }
 
+}
+
+function addNewEmployee() {
+    let checkPhoneNumber = false;
+    let checkIdCard = false;
+    let checkBirthday = false;
+    let checkEmail = false;
+    let checkLevel = false;
+    let checkLocation = false;
+    let checkWork = false;
+    let checkSalary = false;
+
+
+    let emp = new Employee();
+
+    let nameStd = " ";
+
+    emp.setNameEmployee(prompt("Enter Name Employee: "));
+    emp.setNameEmployee(emp.getNameEmployee().trim().toLocaleLowerCase());
+    for (let i = 0; i < emp.getNameEmployee().length; i++) {
+        if (emp.getNameEmployee().charAt(i) === " " && emp.getNameEmployee().charAt(i + 1) === " ") {
+            continue;
+        }
+        if (i === 0 || emp.getNameEmployee().charAt(i - 1) === " ") {
+            nameStd += emp.getNameEmployee().charAt(i).toLocaleUpperCase();
+            continue;
+        }
+        nameStd += emp.getNameEmployee().charAt(i);
+
+    }
+    emp.setNameEmployee(nameStd);
+
+    do {
+        emp.setPhoneNumberEmployee(prompt("Enter Phone Number Employee: "));
+        if (validatePhoneNumber.test(emp.getPhoneNumberEmployee())) {
+            checkPhoneNumber = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkPhoneNumber);
+
+    do {
+        emp.setIdCardEmployee(prompt("Enter Id Card Employee: "));
+        if (validateIdCard.test(emp.getIdCardEmployee())) {
+            checkIdCard = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkIdCard);
+
+    do {
+        emp.setBirthdayEmployee(prompt("Enter Birthday Employee: "));
+        if (validateBirthday.test(emp.getBirthdayEmployee())) {
+            checkBirthday = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkBirthday);
+
+    do {
+        emp.setEmailEmployee(prompt("Enter Email Employee: "));
+        if (validateEmail.test(emp.getEmailEmployee())) {
+            checkEmail = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkEmail);
+
+    do {
+        emp.setLevelEmployee(prompt("Enter Level Employee(Trung Cap,Cao Dang,Dai Hoc) : "));
+        if (validateLevelEmployee.test(emp.getLevelEmployee().toLowerCase().replace(/ /g, ''))) {
+            checkLevel = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkLevel);
+
+    do {
+        emp.setLocationEmployee(prompt("Enter Location Employee(Le Tan,Phuc Vu,Giam Sat,Giam Doc) : "));
+        if (validateLocationEmployee.test(emp.getLocationEmployee().toLowerCase().replace(/ /g, ''))) {
+            checkLocation = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkLocation);
+
+    do {
+        emp.setSalaryEmployee(prompt("Enter Salary Employee: "));
+        if (validateSalary.test(emp.getSalaryEmployee())) {
+            checkSalary = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkSalary);
+
+    do {
+        emp.setWorkEmployee(prompt("Enter Work Employee(Manager,Sale,Marketing): "));
+        if (validateWorkEmployee.test(emp.getWorkEmployee().toLowerCase().replace(/ /g, ''))) {
+            checkWork = true;
+        } else {
+            alert("vui long nhap lai");
+        }
+
+    } while (!checkWork);
+
+
+    listEmployee.push(emp);
+    displayMainMenu();
+
+}
+
+function displayEmployee() {
+    let result = "";
+    for (let i = 0; i < listEmployee.length; i++) {
+        result += "\n" + (i + 1) + ". " + "Name: " + listEmployee[i].getNameEmployee() +
+            "; ID Card" + listEmployee[i].getIdCardEmployee();
+    }
+    result += "\n" + (listEmployee.length + 1) + ".Back to menu";
+    let chooseDisplayInfoEmployee = prompt(result);
+    if (chooseDisplayInfoEmployee.toString() !== (listEmployee.length + 1).toString()) { //k chon back
+        displayInformationEmployee(Number.parseInt(chooseDisplayInfoEmployee) - 1);
+    } else {
+        displayMainMenu();
+    }
+}
+
+function displayInformationEmployee(index) {
+    alert("Information of employee : " +
+        "\nName : " + listEmployee[index].getNameEmployee() +
+        "\nPhone Number Card : " + listEmployee[index].getPhoneNumberEmployee() +
+        "\nID Card : " + listEmployee[index].getIdCardEmployee() +
+        "\nBirthday : " + listEmployee[index].getBirthdayEmployee() +
+        "\nEmail : " + listEmployee[index].getEmailEmployee() +
+        "\nLevel Employee : " + listEmployee[index].getLevelEmployee() +
+        "\nLocation Employee :" + listEmployee[index].getLocationEmployee() +
+        "\nSalary : " + listEmployee[index].getSalaryEmployee() +
+        "\nWork Employee : " + listEmployee[index].getWorkEmployee() +
+        "\nTotal Salary : " + listEmployee[index].totalSalaryEmployee() +
+        "\nEnter to exit."
+    );
+    displayMainMenu();
 }
 
 
@@ -117,7 +277,7 @@ function addNewCustomer() {
 
     do {
         cus.setAddressCustomer(prompt("Enter Address Customer(Da nang,Hue,Quang Nam): "));
-        if (validateAddressCustomer.test(cus.getAddressCustomer().toLowerCase().replace(/ /g,''))) {
+        if (validateAddressCustomer.test(cus.getAddressCustomer().toLowerCase().replace(/ /g, ''))) {
             checkAddressCustomer = true;
         } else {
             alert("vui long nhap lai");
@@ -184,8 +344,6 @@ function addNewCustomer() {
     } while (!checkTypeService) ;
 
 
-
-
     listCustomers.push(cus);
     displayMainMenu();
 }
@@ -198,6 +356,7 @@ function displayCustomers() {
     }
     result += "\n" + (listCustomers.length + 1) + ".Back to menu";
     let chooseDisplayInfo = prompt(result);
+
     if (chooseDisplayInfo.toString() !== (listCustomers.length + 1).toString()) { //k chon back
         if (!checkDeleteCustomer && !checkDisplayTotalPay) {
             displayInformationCustomer(Number.parseInt(chooseDisplayInfo) - 1);
@@ -218,17 +377,17 @@ function displayCustomers() {
 function displayInformationCustomer(index) {
     if (checkEditCustomer) {
         let chooseInfoEdit = prompt(
-            "1.Name :" + listCustomers[index].getNameCustomer() +
-            "\n2.ID Card :" + listCustomers[index].getIdCard() +
-            "\n3.Birthday :" + listCustomers[index].getBirthdayCustomer() +
+            "1.Name : " + listCustomers[index].getNameCustomer() +
+            "\n2.ID Card : " + listCustomers[index].getIdCard() +
+            "\n3.Birthday : " + listCustomers[index].getBirthdayCustomer() +
             "\n4.Email :" + listCustomers[index].getEmailCustomer() +
-            "\n5.Address :" + listCustomers[index].getAddressCustomer() +
-            "\n6.Type Customer :" + listCustomers[index].getTypeCustomer() +
-            "\n7.Discount :" + listCustomers[index].getDiscount() +
-            "\n8.Number Of Accomanying :" + listCustomers[index].getNumberOfAccompanying() +
-            "\n9.Type Room :" + listCustomers[index].getTypeRoom() +
-            "\n10.Rent Day :" + listCustomers[index].getRentDays() +
-            "\n11.Type Service :" + listCustomers[index].getTypeService() +
+            "\n5.Address : " + listCustomers[index].getAddressCustomer() +
+            "\n6.Type Customer : " + listCustomers[index].getTypeCustomer() +
+            "\n7.Discount : " + listCustomers[index].getDiscount() +
+            "\n8.Number Of Accomanying : " + listCustomers[index].getNumberOfAccompanying() +
+            "\n9.Type Room : " + listCustomers[index].getTypeRoom() +
+            "\n10.Rent Day : " + listCustomers[index].getRentDays() +
+            "\n11.Type Service : " + listCustomers[index].getTypeService() +
             "\n12.Back. ");
         if (chooseInfoEdit.toString() !== "12") { //ko chon back
             editInformationCustomer(index, Number.parseInt(chooseInfoEdit) - 1); //
@@ -239,17 +398,17 @@ function displayInformationCustomer(index) {
         }
     } else {
         alert("Information of customers : " +
-            "\nName :" + listCustomers[index].getNameCustomer() +
-            "\nID Card :" + listCustomers[index].getIdCard() +
-            "\nBirthday :" + listCustomers[index].getBirthdayCustomer() +
-            "\nEmail :" + listCustomers[index].getEmailCustomer() +
-            "\nAddress :" + listCustomers[index].getAddressCustomer() +
-            "\nType Customer :" + listCustomers[index].getTypeCustomer() +
-            "\nDiscount :" + listCustomers[index].getDiscount() +
-            "\nNumber Of Accomanying :" + listCustomers[index].getNumberOfAccompanying() +
-            "\nType Room :" + listCustomers[index].getTypeRoom() +
-            "\nRent Day :" + listCustomers[index].getRentDays() +
-            "\nType Service :" + listCustomers[index].getTypeService()
+            "\nName : " + listCustomers[index].getNameCustomer() +
+            "\nID Card : " + listCustomers[index].getIdCard() +
+            "\nBirthday : " + listCustomers[index].getBirthdayCustomer() +
+            "\nEmail : " + listCustomers[index].getEmailCustomer() +
+            "\nAddress : " + listCustomers[index].getAddressCustomer() +
+            "\nType Customer : " + listCustomers[index].getTypeCustomer() +
+            "\nDiscount : " + listCustomers[index].getDiscount() +
+            "\nNumber Of Accompanying : " + listCustomers[index].getNumberOfAccompanying() +
+            "\nType Room : " + listCustomers[index].getTypeRoom() +
+            "\nRent Day : " + listCustomers[index].getRentDays() +
+            "\nType Service : " + listCustomers[index].getTypeService()
         );
         displayMainMenu();
     }
@@ -304,12 +463,12 @@ function editInformationCustomer(index, editIndex) {  //
 function deleteCustomer(index) {
     let chooseConfirm = prompt(
         "Are you sure delete Customer : Name " + listCustomers[index].getNameCustomer() +
-        ",ID card" + listCustomers[index].getIdCard() +
+        ",ID card " + listCustomers[index].getIdCard() +
         "\n1.Yes\n2.No"
     );
     if (chooseConfirm === "1") {
         listCustomers.splice(index, 1);
-        alert("delete complete");
+        alert("Delete complete");
     }
     checkDeleteCustomer = false;
     displayMainMenu();
@@ -322,7 +481,7 @@ function chooseDisplayTotalPay() {
 }
 
 function displayTotalPay(index) {
-    alert("Tổng tiền của " +listCustomers[index].getNameCustomer() +" là : "+listCustomers[index].totalPays() +" lúa Trump");
+    alert("Tổng tiền của " + listCustomers[index].getNameCustomer() + " là : " + listCustomers[index].totalPays() + " lúa Trump");
     checkDisplayTotalPay = false;
     displayMainMenu();
 
